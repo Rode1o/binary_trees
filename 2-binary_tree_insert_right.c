e98ff5bc6647d65460c3a368f60f875836ff3968
jj131204 <jj131204@gmail.com>
2021-07-20 19:51:05 -0500

    tests

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -18,15 +18,12 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (!new_node)
 		return (NULL);
 
-	if (new_node->right)
+	if (parent->right)
 	{
 		new_node->right = parent->right;
 		parent->right->parent = new_node;
 	}
 
-	else
-	{
-		parent->right = new_node;
-	}
+	parent->right = new_node;
 	return (new_node);
 }
--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -10,6 +10,12 @@ void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree != NULL)
 	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
 		free(tree);
+	}
+	else
+	{
+		return;
 	}
 }
